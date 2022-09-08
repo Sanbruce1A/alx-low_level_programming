856ff7b7de90899da93521e63126172cf81747e6
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-08 12:03:38 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  ** main - entry point
  ** Return: 0*
  *
- **/
+*/
 int main(void)
-	printf("\"with proper grammar, but the outcome is a piece of art,");
-	return (0);
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
