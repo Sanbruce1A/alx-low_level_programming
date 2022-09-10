2370b70d3dd8e695bda6cc78d542b7cc9dd955b9
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-09 17:30:17 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,27 +2,36 @@
 
 /**
  ** main - Prints 3 combination of numbers
+ *
  ** Return: Always (Success)
+ *
  */
+
 int main(void)
 
 {
-int c, i, k;
+
+int c, i, k, g;
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
 {
 for (k = '0'; k <= '9'; k++)
 {
-if (c < i && i < k)
+for (g = '0'; g <= '9'; g++)
+{
+if (c < i && i < k && k < g)
 {
 putchar(c);
 putchar(i);
 putchar(k);
+putchar(g);
 if (c != '7')
 {
 putchar(',');
 putchar(' ');
+putchar(' ');
+}
 }
 }
 }
@@ -30,4 +39,5 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
+
 }
