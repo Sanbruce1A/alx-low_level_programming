feffb9740199428989dd222bd685308314b884fd
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-13 08:35:52 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+** main - Prints 3 combination of numbers
+** Return: Always (Success)
+*/
+
+int main(void)
+
+{
+void print_alphabet(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+}
+}
