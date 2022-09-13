9f20ded9f59c80784f907355221e80747b5904cd
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-13 11:57:26 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,13 +21,8 @@ j = 0;
 while (j < 60)
 {
 _putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-_putchar('\n');
-j++;
-}
-}
-}
 
+i++;
+}
+}
+}
