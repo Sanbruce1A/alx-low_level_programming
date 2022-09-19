2b3b9167bc9491534508ece8402aba0d560d5734
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-19 05:25:13 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _strlen - length of a string
+ * @s: a pointer given by the main function
+ * Return:lenght
+ */
+
+int_strlen(char *s)
+{
+
+	int lenght = 0;
+
+	while (s[length] != '\0')
+
+	{
+		lenght++;
+	}
+	return (lenght);
+}
