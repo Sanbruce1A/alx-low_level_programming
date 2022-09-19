c574d471f480b9225525a8928c99472ab88b88a1
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-19 07:46:43 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,11 +14,10 @@ void puts2(char *str)
 	{
 		if (counter % 2 == 0)
 
-			_putchar(str[counter] != '\0')
-			{
-				if (counter % 2 == 0)
+		{
 
-					_putchar(str[counter]);
+		_putchar(str[counter]);
+
 				counter++;
 			}
 		_putchar(10);
