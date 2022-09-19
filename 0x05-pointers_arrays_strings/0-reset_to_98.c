c15007e02c3339e2fc177df9ca3c691003853267
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-19 04:01:22 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include  <main.h>
+
+/**
+ * reset_to_98 -  updates the value of n points to 98
+ * @n: void reset_to_98
+ * Description: updates the value it points
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
