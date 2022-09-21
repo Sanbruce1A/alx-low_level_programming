2ddfb9d4c9bca690627978c28510c0030ea2bc0e
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-21 06:48:04 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate two strings and minus from source
- * @dest: First string
- * @src: Second string
- * @n: byte to see from output
+ * _strncpy - copies a string by number of character
+ * @dest: a poiner to a character
+ * @src: a pointer to a character
+ * @n: value
  * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
-{
-	int a = 0, p = 0;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[p] != '\0' && n > 1)
-	{
-		dest[a] = src[p];
-		a++;
-		p++;
-	}
-	if (n > 0)
-	{
-		dest[a] = '\n';
-	}
-	return (dest);
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
 }
+	while (i < n)
+{
+	dest[i] = '\0';
+	i++;
+}
+	return (dest);
+	}
