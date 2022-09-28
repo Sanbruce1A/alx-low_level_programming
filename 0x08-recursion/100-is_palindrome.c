63ef0533c7b149cfb6179e2ef8de1ee6ead9b7de
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-28 09:42:31 -0700

    Task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,10 +2,10 @@
 
 /**
  * palind2 - obtains length of a
- *@a: string
- *@l: integer to count length
- *Return: On success 1.
- *On error, -1 is returned, and errno is set appropriately
+ * @a: string
+ * @l: integer to count length
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int palind2(char *a, int l)
@@ -16,16 +16,16 @@ int palind2(char *a, int l)
 }
 
 /**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately.
+ * palind3 - compares strings vs string reverse
+ *  @a: string
+ *  @l: length
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
  */
 
 int palind3(char *a, int l)
 {
-	if (*a != *(a + 1))
+	if (*a != *(a + l))
 		return (0);
 	else if (*a == 0)
 		return (1);
@@ -44,6 +44,5 @@ int is_palindrome(char *s)
 	int l;
 
 	l = palind2(s, 0);
-	return (palind3(s, 1));
-
+	return (palind3(s, l));
 }
