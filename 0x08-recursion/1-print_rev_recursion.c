cc5b62d0ae1855ff35bda876e1555694f5e8fa49
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-28 04:53:11 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: pointer to the string
+ * Return: Always
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
