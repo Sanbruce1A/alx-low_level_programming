f89df659e92a033a1409d3d12785c2867894046a
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-30 08:42:29 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints a program
+ * @argc: first argument
+ * @argv: argument
+ * Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+	void argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
