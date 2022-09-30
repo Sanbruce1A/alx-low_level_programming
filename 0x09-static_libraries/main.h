883bd73a8c40311d3886d93acb35dc38173a40eb
Sanbruce1A <saniimoleayo@gmail.com>
2022-09-30 08:16:37 -0700

    Task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +40,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+#endif
