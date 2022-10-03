77b69d8541d4050c4e0dc1bd7b02e74ce9ec12c8
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-03 13:33:30 -0700

    Task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -23,20 +23,18 @@ free(grid);
  * @str: string.
  * Return: pointer of an array of integers
  */
-
 char **strtow(char *str)
 {
-char **aout;
-unsigned int c, height, i, j, a1;
-
+	char **aout;
+	unsigned int c, height, i, j, a1;
 if (str == NULL || *str == '\0')
 return (NULL);
 for (c = height = 0; str[c] != '\0'; c++)
-if (str[c] != ' ' && (str[c + 1] != ' ' || str[c + 1] == '\0'))
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 height++;
 aout = malloc((height + 1) * sizeof(char *));
 if (aout == NULL || height == 0)
-	{
+{
 free(aout);
 return (NULL);
 }
@@ -61,6 +59,6 @@ for (j = 0; a1 <= c; a1++, j++)
 aout[i][j] = str[a1];
 aout[i][j] = '\0';
 }
-	aout[i] = NULL;
+aout[i] = NULL;
 return (aout);
 }
