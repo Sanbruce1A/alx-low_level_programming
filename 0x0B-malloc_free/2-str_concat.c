f72937792b238449dd43d18fe0978e90ed8258c6
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-03 05:12:45 -0700

    Task 1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - return a pointer to newly allocated memory
+ * @str: a string given by main
+ * Return: the pointer or null
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	unsigned int night, i;
+
+	/* confirm if str is null*/
+
+	if (str == NULL)
+
+	{
+		return (NULL);
+	}
+
+	night = 0;
+
+	/* while str is not equal to 0*/
+
+	while (str[night] != '\0')
+	{
+		night++;
+	}
+
+	ptr = malloc(sizeof(char) * (night + 1));
+
+	/*if malloc is null*/
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	/* if malloc is not null start a for loop*/
+
+	for (i = 0; i < night; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[night] = '\0';
+	return (ptr);
+}
