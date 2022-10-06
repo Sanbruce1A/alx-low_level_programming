70aca0c328d0a0d67903bfdf8ffa3004c1c5bdd2
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-06 04:32:04 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory
+ * @b: number of memory to be allocated
+ * Return: Void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *spike;
+spike = malloc(b);
+if (spike == NULL)
+{
+	exit(98);
+}
+return (night);
+}
