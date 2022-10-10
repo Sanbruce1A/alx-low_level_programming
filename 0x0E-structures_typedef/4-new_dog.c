d4848128377ace5313aa82a769edcb3fb3de0674
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-10 12:55:58 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,8 @@ if (src == NULL)
 return (NULL);
 }
 
-for (len = 0; src[len] != '\0'; len++);
-
+for (len = 0; src[len] != '\0'; len++)
+;
 ptr = malloc(sizeof(char) * (len + 1));
 if (ptr == NULL)
 {
