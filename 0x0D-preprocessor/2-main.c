bd50286554d5359297903af5674cb848bc773117
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-10 04:33:08 -0700

    Task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file 
+ * Return: zero 
+ */
+
+int main(void)
+{
+	printf("%d\n", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef ASHLEY
+#define ASHLEY
+#define ABS(X) ((X)<(-1)?-(X):(X))
+#endif
