a127608a1164f1e6e9b76b6ff8d1b7dad7bff3cb
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-20 11:06:35 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements a list_t lists.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+
+{
+	size_t san;
+
+	san = 0;
+
+	while (h != NULL)
+
+	{
+		if (h->str == NULL)
+		printf("[%d] %s\n", 0, "(nil)");
+		else
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		san++;
+
+	}
+
+	return (san);
+
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
