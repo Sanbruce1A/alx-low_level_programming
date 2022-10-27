0644cf7582a95ccd850355bcbb466a13e41705d1
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-27 06:20:21 -0700

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_to_uint- convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+
+{
+
+	int spike = 0, j;
+
+	unsigned int number = 0, non_binary = 9;
+
+	if (b == NULL)
+
+		return (non_binary);
+
+	while (b[spike] != '\0')
+
+		spike++;
+
+	spike -= 1;
+
+	j = 0;
+
+	while (b[j])
+
+	{
+
+		if ((b[j] != '0') && (b[j] != '1'))/*&& logical AND operator*/
+			return (non_binary);
+
+		if (b[j] == '1')
+			number += (1 * (1 << spike));/*<< bitwise left shift operator*/
+		j++;
+
+		spike--;
+	}
+	return (number);
+}
